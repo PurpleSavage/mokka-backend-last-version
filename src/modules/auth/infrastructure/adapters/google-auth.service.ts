f852adb9c6e70fbf7f07db70ea017ada4d4f5811
PurpleSavage <jzuritapalomino@gmail.com>
2026-02-11 20:17:06 -0500

    agregar pino logger al servicio de auth con google

--- src/modules/auth/infrastructure/adapters/google-auth.service.ts
+++ src/modules/auth/infrastructure/adapters/google-auth.service.ts
@@ -1,12 +1,18 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import {  HttpStatus, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { OAuth2Client } from 'google-auth-library'; 
 import { GooglePort } from "../../application/ports/google.port";
 import { GoogleUserData } from "../types/google-response";
+import { MokkaError } from "src/shared/errors/mokka.error";
+import { ErrorPlatformMokka } from "src/shared/infrastructure/enums/error-detail-types";
+import { PinoLogger } from "nestjs-pino";
 @Injectable()
 export class GoogleAuthService implements GooglePort{
     private readonly client: OAuth2Client;
-    constructor(private configService: ConfigService){
+    constructor(
+        private configService: ConfigService,
+        private readonly logger: PinoLogger
+    ){
         const googleClientId= this.configService.get<string>('GOOGLE_CLIENT_ID')
         const googleClientIdSecret= this.configService.get<string>('GOOGLE_CLIENT_SECRET')
         this.client= new OAuth2Client(
@@ -19,17 +25,19 @@ export class GoogleAuthService implements GooglePort{
         // Verificar el token con Google Auth Library
         const ticket = await this.client.verifyIdToken({
             idToken: googleToken,
-            audience: process.env.GOOGLE_CLIENT_ID,
+            audience: this.configService.get<string>('GOOGLE_CLIENT_ID'),
         });
 
         const payload = ticket.getPayload();
         
         if (!payload) {
-            throw new HttpException({
+            throw new MokkaError({
+                message: 'Error to login account',
+                errorType: ErrorPlatformMokka.GOOGLE_UNAUTHORIZED,
                 status: HttpStatus.UNAUTHORIZED,
-                error:'Invalid Google token payload',
-                errorType:'Google_ERROR'
-            },HttpStatus.UNAUTHORIZED)
+                details: 'Invalid Google token payload'
+            })
+                        
         }
 
         // Extraer datos del usuario
@@ -40,12 +48,19 @@ export class GoogleAuthService implements GooglePort{
             picture: payload.picture,
         };
         } catch (error) {
-            console.error('Google token verification failed:', error)
-            throw new HttpException({
-                status: HttpStatus.UNAUTHORIZED,
-                error:'Invalid Google token payload',
-                errorType:'Google_ERROR'
-            },HttpStatus.UNAUTHORIZED)
+            this.logger.error(
+                { 
+                    stack: error instanceof Error ? error.stack : undefined, 
+                    message: 'GoogleAuth failed' 
+                }, 
+                'Google token verification failed'
+            );
+            throw new MokkaError({
+                message: 'Error to login account',
+                errorType: ErrorPlatformMokka.GOOGLE_FAILED,
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                details: 'Failed server to validate token'
+            })
         }
     }
 }
