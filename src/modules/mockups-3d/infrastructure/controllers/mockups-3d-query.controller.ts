b782d27409dec65c2dc9afb63126c6ed70e4a401
PurpleSavage <jzuritapalomino@gmail.com>
2026-03-29 22:30:01 -0500

    agregar guard para listar todos los modelso 3d

--- src/modules/mockups-3d/infrastructure/controllers/mockups-3d-query.controller.ts
+++ src/modules/mockups-3d/infrastructure/controllers/mockups-3d-query.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, HttpCode, HttpStatus, Query} from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, Query, UseGuards} from "@nestjs/common";
 import { Throttle } from "@nestjs/throttler";
 //import { AccesstokenGuard } from "src/guards/tokens/access-token.guard";
 import { List3DModelsUseCase } from "../../application/use-cases/list-3d-models.use-case";
 import { ListResourcesDto } from "../../application/dtos/requests/list-resources.dto";
+import { AccesstokenGuard } from "src/guards/tokens/access-token.guard";
 
 @Controller({
     version:'1',
@@ -13,7 +14,7 @@ export class Mockups3DQueryController{
         private readonly list3dModelsUseCase:List3DModelsUseCase
     ){}
     @Throttle({ default: { limit: 10, ttl: 60000 } })
-    //@UseGuards(AccesstokenGuard)
+    @UseGuards(AccesstokenGuard)
     @Get('all')
     @HttpCode(HttpStatus.OK)
     getModels(
